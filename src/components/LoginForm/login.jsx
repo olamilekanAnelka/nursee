14c964839e764568e6a00949a354ded07d610269
olamilekanAnelka <ogunkanbiolamilekan@gmail.com>
2023-06-17 07:45:49 +0100

    updated my nursee folder

--- src/components/LoginForm/login.jsx
+++ src/components/LoginForm/login.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
 import '../LoginForm/login.css'
+import { useNavigate } from 'react-router-dom'
 
 const Login = () => {
+         const navigate= useNavigate();
+         const [nameOrEmail, setNameOrEmail] = useState('');
+         const [password, setPassword] = useState('');
+
+
+         const handleSubmit = (e) => {
+          e.preventDefault();
+          console.log('Name or Email:', nameOrEmail);
+          console.log('Password:', password);
+          navigate('/contactUs');
+        };
+
+
   return (
     <div className='loginWrapper'>
-       <form  className="formParent">
+       <form  className="formParent" onSubmit={handleSubmit}>
         <div className="topLoginForm">
-           <input id='mail' type="text" placeholder='username or email' />
-           <input id='password' type="password" placeholder='password' />
+           <input id='mail' type="text" placeholder='username or email' value={nameOrEmail} onChange={(e) => setNameOrEmail(e.target.value)}  />
+           <input id='password' type="password" placeholder='password'value={password} onChange={(e) => setPassword(e.target.value)}  />
             <div className='checkBox'>
             <label for="termsCheckbox">
           <input type="checkbox" id="termsCheckbox" required/>
--- src/components/RegistrationForm/reg.jsx
+++ src/components/RegistrationForm/reg.jsx
@@ -1,26 +1,47 @@
-import React from 'react'
+import React, {useState} from 'react'
 import '../RegistrationForm/reg.css'
+import { useNavigate } from 'react-router-dom';
 
 const Reg = () => {
+  const navigate = useNavigate();
+  const [title, setTitle] = useState('');
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [password, setPassword] = useState('');
+  const [courseType, setCourseType] = useState('');
+  const [contactMethod, setContactMethod] = useState('');
+  const [hours, setHours] = useState('');
+  const [agreed, setAgreed] = useState(false);
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate('/login')
+  };
+
   return (
     <div className='regWrapper'>
-      <form className='regform'>
+      <form className='regform'onSubmit={handleSubmit}>
         <div className="regdiv">
         <h3>EDUCATION APPOINTMENT FORM</h3>
   <div class="regInput">
-  <input type="text" class="form-control" id="validationCustom01"  placeholder='Title' /> 
+  <input type="text" class="form-control" id="validationCustom01"  placeholder='Title' value={title} onChange={(e) => setTitle(e.target.value)} /> 
   </div>
   <div class="regInput">
-  <input type="text" class="form-control" id="validationCustom02"  placeholder='Name'/>
+  <input type="text" class="form-control" id="validationCustom02"  placeholder='Name' value={name} onChange={(e) => setName(e.target.value)}/>
     </div>
     <div class="regInput">
-  <input type="text" class="form-control" id="validationCustom02"  placeholder='Email'/>
+  <input type="text" class="form-control" id="validationCustom02"  placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)}/>
     </div>
     <div class="regInput">
-  <input type="text" class="form-control" id="validationCustom02"  placeholder='Phone Number'/>
+  <input type="text" class="form-control" id="validationCustom02"  placeholder='Phone Number' value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)}/>
+    </div>
+    <div class="regInput">
+  <input type="password" class="form-control" id="validationCustom02"  placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)}/>
     </div>
     <div class="">
-    <select class="form-select" id="validationCustom04" required>
+    <select class="form-select" id="validationCustom04" required  value={courseType} onChange={(e) => setCourseType(e.target.value)}>
       <option selected disabled value="">Course type</option>
       <option>...</option>
     </select>
@@ -30,13 +51,13 @@ const Reg = () => {
     <h4>How would you like how to be located</h4>
   </div>
   <div class="">
-    <select class="form-select" id="validationCustom04" required>
+    <select class="form-select" id="validationCustom04" required  value={contactMethod} onChange={(e) => setContactMethod(e.target.value)}>
       <option selected disabled value="">By phone</option>
       <option>...</option>
     </select>
   </div>
   <div class="">
-    <select class="form-select" id="validationCustom04" required>
+    <select class="form-select" id="validationCustom04" required value={hours} onChange={(e) => setHours(e.target.value)}>
       <option selected disabled value="">Hours: 8am 10pm</option>
       <option>...</option>
     </select>
@@ -44,7 +65,7 @@ const Reg = () => {
   
   <div class="">
     <div class="form-check">
-      <input class="form-check-input" type="checkbox" value="" id="invalidCheck" required/>
+      <input class="form-check-input" type="checkbox" value="" id="invalidCheck" required checked={agreed} onChange={(e) => setAgreed(e.target.checked)}/>
       <label class="form-check-label" for="invalidCheck">
       <p>I agree to the <span>Terms and Conditions</span></p>
       </label>
